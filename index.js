9ad4c76077340cd2afff072c39001dd1dc9a34e6
DanH0210 <mnh02danh10@gmail.com>
2024-01-20 16:28:57 -0700

    Implement fight(str player, str comp) function
    fight function will take in two string parameters
    playerSelection and computerSelection
    it will compare the choice and console.log out
    the result and return either the winner or "tie"

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 function getComputerChoice() {
-    let choices = ["rock", "paper", "scissors"];
+    let choices = ["Rock", "Paper", "Scissors"];
     randomIndex = Math.floor(Math.random() * 3);
     return choices[randomIndex];
 }
+
+function fight(playerSelection, computerSelection) {
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    if (playerSelection === computerSelection) {
+        console.log(`You tied! Both of you chose ${playerSelection}`)
+        return "tie";
+    }
+    if (playerSelection === "Rock" && computerSelection === "Paper") {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        return "computer";
+    }
+    if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+        return "player";
+    }
+    if (playerSelection === "Paper" && computerSelection === "Rock") {
+        console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+        return "player";
+    }
+    if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        return "computer";
+    }
+    if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        return "computer";
+        
+    }
+    if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+        return "player";
+    }
+}
+
